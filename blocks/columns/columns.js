86b9a37e33a7db6a4ea7f2e0785368a68b8ae6bf
lakshya aggarwal <laggarwal@adobe.com>
2026-05-07 17:13:50 +0530

    Refactor columns block to improve nested block handling and remove redundant code
    - Updated the decorate function to utilize decorateBlock and loadBlock for better management of nested blocks.
    - Removed unnecessary checks and logic related to empty divs and AEM authoring environment.
    - Enhanced overall functionality and maintainability of the columns block.

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,24 +1,21 @@
-import { getAemAuthorEnv } from '../../scripts/configs.js';
+import { decorateBlock, loadBlock } from '../../scripts/aem.js';
 
-const AEM_DIV_EXTRA_CONTENT = ['text, grid-layout', 'icon, icon-layout', 'promo, promo-layout'];
+export default async function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
 
-export default function decorate(block) {
-  // this is for UE to use the same columns block no matter the layout
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < block.classList.length; i++) {
-    if (block.classList[i].includes('grid-placeholder-icons')) {
-      block.classList.remove('grid-placeholder-icons');
-      block.classList.add('grid');
-    }
-  }
-  if (block.firstElementChild && block.firstElementChild.children) {
-    const cols = [...block.firstElementChild.children];
-    block.classList.add(`columns-${cols.length}-cols`);
-  }
+  const nestedBlocks = [];
 
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
+      [...col.children].forEach((child) => {
+        decorateBlock(child);
+        if (child.classList.contains('block')) {
+          nestedBlocks.push(child);
+        }
+      });
+
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
@@ -27,23 +24,11 @@ export default function decorate(block) {
           picWrapper.classList.add('columns-img-col');
         }
       }
-      // this is to remove empty <div></div> because of UE
-      // using the same columns block, only in PREVIEW/PUBLISH
-      if (!col.textContent.trim()) {
-        row.remove();
-      }
-      // this is to remove the info-only <div></div> listing the style
-      // chosen in UE because it's not an actual 'content' block
-      if (AEM_DIV_EXTRA_CONTENT.includes(col.textContent.trim())) {
-        col.remove();
-      }
     });
   });
 
-  const isAemAuthor = getAemAuthorEnv();
-  if (isAemAuthor && /^\s*\n\s*$/.test(block.innerHTML)) { // block.innerHTML.trim() === '' && block.childNodes && block.childNodes.length === 0) {
-    const authorBlock = document.createElement('div');
-    authorBlock.textContent = 'Columns JS Object container for enrichment';
-    block.appendChild(authorBlock);
+  for (let i = 0; i < nestedBlocks.length; i += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    await loadBlock(nestedBlocks[i]);
   }
 }
